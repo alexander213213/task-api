798684ec5fbbc17acc519e2e58dbb54ad95afa5f
Alexander Gracilla <alexandergracilla@gmail.com>
2026-02-22 08:38:11 +0800

    refactor(error): fix payload for better uniformity and remove logs

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -2,20 +2,20 @@ import { NextFunction, Request, Response, } from "express"
 import { PrismaClientKnownRequestError } from "../../generated/prisma/internal/prismaNamespace"
 
 export function errorHandler(err: unknown, req: Request, res: Response, next: NextFunction) {
-    console.error(err)
+    // console.error(err)
     if (err instanceof PrismaClientKnownRequestError) {
         if (err.code === "P2002") {
             const fields = err.meta?.target as string[] | undefined
 
             if (fields?.includes("email")) {
-                return res.status(409).json({ error: "Email already exists" })
+                return res.status(409).json({ ok: false, error: "Email already exists" })
             }
 
             if (fields?.includes("username")) {
-                return res.status(409).json({ error: "Username already exists" })
+                return res.status(409).json({ ok: false, error: "Username already exists" })
             }
 
-            return res.status(409).json({ error: "Unique constraint failed" })
+            return res.status(409).json({ ok: false, error: "Unique constraint failed" })
         }
     }
 
