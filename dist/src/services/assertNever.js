ce70054bb5c81dcfeed8bbe5c8c9bd0eb9a6ec84
Alexander Gracilla <alexandergracilla@gmail.com>
2026-03-01 20:44:37 +0800

    fix(dist): remove dist

--- dist/src/services/assertNever.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.assertNever = assertNever;
-function assertNever(x) {
-    throw new Error("This case never throws");
-}
