ad6c8302e1f61c5e0625326ade95529bd4b34573
Alexander Gracilla <alexandergracilla@gmail.com>
2026-03-01 20:47:12 +0800

    fix(index): remove localhost url in logs

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT ?? 3000
 
 
 app.listen(port, (error) => {
-    console.log(`Server running at http://localhost:${port}`)
+    console.log(`Server running at ${port}`)
 
     if (error) {
         console.log(error.message)
